13e23c5967b3ce34ed6b658bd2be6021dedf0cb4
Zen-nojen <jiyoyo1234@naver.com>
2025-02-13 18:27:25 +0900

    patch, delete 추가

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import * as dotenv from "dotenv";
 
 dotenv.config(); // 필수!
@@ -15,10 +15,35 @@ app.get("/users", async (req, res) => {
 }); // await 는 async 를 써야 호출 가능!
 
 app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  // Destructuring assignment
   const user = await prisma.user.findUnique({
-    where: { id: req.params.id },
+    where: { id },
   });
   res.send(user);
+});
+
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  const user = await prisma.user.create({ data: req.body });
+  res.status(201).send(user);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: { id },
+  });
+  req.send("Success delete");
 });
 
 app.listen(process.env.PORT || 3000, () =>
